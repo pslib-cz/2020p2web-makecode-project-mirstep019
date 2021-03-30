1cbe00d66fa4f2215d43e1666cb883d265ef475e
mirstep019 <71689639+mirstep019@users.noreply.github.com>
2021-03-30 11:27:39 +0200

    work01

--- main.ts
+++ main.ts
@@ -1,1 +1,26 @@
- 
+scene.setBackgroundColor(9);
+let mySprite = sprites.create(img`
+    . . . . . . . 2 2 2 1 1 2 . . .
+    . . . . . . 2 2 2 2 2 2 2 2 . .
+    . . . . . f f f f d 1 f d . . .
+    . . . . f f d f f d 1 f d . . .
+    . . . . f f d d d d d d d d . .
+    . . . . . f d d d d d f d . . .
+    . . . . 8 8 f 8 8 8 8 . . . . .
+    . . . 8 8 8 f 8 8 f 8 8 8 . . .
+    . . 8 8 8 8 f f f f 8 8 8 8 . .
+    . . d d 8 f 5 f f 5 f 8 d d . .
+    . . d d d f f f f f f d d d . .
+    . . d d f f f f f f f f d d . .
+    . . . . f f f . . f f f . . . .
+    . . . 2 1 2 . . . . 2 1 2 . . .
+    . . 2 1 2 2 . . . . 2 2 1 2 . .
+    . . . . . . . . . . . . . . . .
+`)
+mySprite.setPosition(20, 165)
+controller.moveSprite(mySprite, 100, 100)
+scene.cameraFollowSprite(mySprite)
+tiles.setTilemap(assets.tilemap`level0`)
+
+//music.playMelody("D4 D4 D4 A4 G#4 G F D F G C4 C4 D A G# G F D F G B3 B3 D4 A4 G#4 G4 F4 D4 F4 G4 A#3 A#3 D4 A4 G#4 G4 F4 D4 F4 G4", 300);
+
---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level0":
+            case "level6":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000700000000000000000000000000000104000001030303000000070700010306050000020606060103030303030606060500000206060602060606060606060605000002060606020606060606060606050000020606060206060606060606060500000206060602060606060606060605000002060606`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tilePath1,sprites.castle.tilePath4,sprites.castle.tilePath2,sprites.castle.tilePath3,sprites.castle.tilePath6,sprites.castle.tilePath5,sprites.builtin.forestTiles0], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
